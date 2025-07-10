4242c4c5d71b5a19821503b56cd50f51084beca2
ak204479@gmail.com <ak204479@gmail.com>
2025-07-11 00:46:13 +0530

    Inital Commit

---
+++ online-quiz-platform-parent/src/main/java/com/onlinequiz/online_quiz_platform_parent/App.java
@@ -0,0 +1,13 @@
+package com.onlinequiz.online_quiz_platform_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
